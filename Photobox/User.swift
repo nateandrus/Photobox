737db699161fa0a9a072e290504b8e235e6b4edc
Nathan Andrus <npandrus@Nathans-MacBook-Air.local>
2019-03-11 16:36:54 -0600

    Setting up the main feed storyboard

---
+++ Photobox/User.swift
@@ -0,0 +1,46 @@
+//
+//  User.swift
+//  Photobox
+//
+//  Created by Nathan Andrus on 3/11/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class User {
+    
+    static let typeKey = "User"
+    fileprivate static let usernameKey = "username"
+    fileprivate static let profileImageKey = "profileImage"
+    fileprivate static let creatorReferenceKey = "creatorReference"
+
+    var username: String
+    var profileImage: UIImage
+    let ckRecord: CKRecord.ID
+    let creatorReference: CKRecord.Reference
+    
+    init(username: String, profileImage: UIImage, ckRecord: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString), creatorReference: CKRecord.Reference) {
+        self.username = username
+        self.profileImage = profileImage
+        self.ckRecord = ckRecord
+        self.creatorReference = creatorReference
+    }
+    
+    convenience init?(record: CKRecord) {
+        guard let username = record[User.usernameKey] as? String,
+            let profileImage = record[User.profileImageKey] as? UIImage,
+            let creatorReference = record[User.creatorReferenceKey] as? CKRecord.Reference else { return nil}
+        self.init(username: username, profileImage: profileImage, ckRecord: record.recordID, creatorReference: creatorReference)
+    }
+}
+
+extension CKRecord {
+    convenience init?(user: User) {
+        self.init(recordType: User.typeKey, recordID: user.ckRecord)
+        setValue(user.username, forKey: User.usernameKey)
+        setValue(user.profileImage, forKey: User.profileImageKey)
+    }
+}
+
---
+++ Photobox/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  Photobox
+//
+//  Created by Nathan Andrus on 3/11/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import Foundation
